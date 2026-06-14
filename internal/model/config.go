07c3f6544d81daa856833f9e8e94a6173cdd8ea4
allenbiji <georgeallen378@gmail.com>
2026-06-14 16:07:45 +0530

    feat: add enum

--- internal/model/config.go
+++ internal/model/config.go
@@ -1,5 +1,25 @@
 package model
 
+type Severity string
+
+const (
+	SeverityInfo    Severity = "info"
+	SeverityWarning Severity = "warning"
+	SeverityBlocker Severity = "blocker"
+)
+
+type CheckType string
+
+const (
+	TypeEnvExists       CheckType = "env_exists"
+	TypeCommandExists   CheckType = "command_exists"
+	TypeFileExists      CheckType = "file_exists"
+	TypeDirectoryExists CheckType = "directory_exists"
+	TypeHttpReachable   CheckType = "http_reachable"
+	TypeTcpReachable    CheckType = "tcp_reachable"
+	TypePortFree        CheckType = "port_free"
+)
+
 // this is the type for each of the checks
 type CheckConfig struct {
 	Name     string            `yaml:"name"`
